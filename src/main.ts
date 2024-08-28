fb4ea3032692687d96775d358da5dbf13fbf72c7
OmkarBhosale07 <omkarbhosale5484@gmail.com>
2024-08-29 01:22:47 +0530

    Code Update

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,20 @@
  * @param AMOUNT - It stores the amount to be transferred, which is a number in this case.
  * @returns - The function returns the base64 image of the QR code for the given UPI ID and AMOUNT.
  */
+import QRCode from "qrcode";
 
-const QRCode = require("qrcode");
-
-type FunctionParams = {
+interface FunctionParams {
   UPI_ID: string;
   AMOUNT: number;
-};
+}
 
 const generateQR = async ({
   UPI_ID,
   AMOUNT,
 }: FunctionParams): Promise<string> => {
-  const upiURL: string = `upi://pay?pa=${UPI_ID}&am=${AMOUNT}`;
+  const upiURL = `upi://pay?pa=${UPI_ID}&am=${AMOUNT}`;
   try {
     const qrCodeDataURL = await QRCode.toDataURL(upiURL);
-
-    // const base64String = qrCodeDataURL.split(",")[1];
-
     return qrCodeDataURL;
   } catch (error) {
     console.error("Error generating QR code:", error);
